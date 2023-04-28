d595edc0f31c9f8e1195cd774a5eb470f1a73320
Aliaksandr Rusak <Aliaksandr.Rusak@ihsmarkit.com>
2023-04-28 19:07:12 +0200

    Task5

--- 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,17 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        private static List<int> _collection = new List<int>();
+        private static ReaderWriterLockSlim _rwLock = new ReaderWriterLockSlim();
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -18,8 +24,40 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine();
 
             // feel free to add your code
+            var rnd = new Random();
+            
+            var readThread = new Thread(() =>
+            {
+                
+            });
+            readThread.IsBackground= true;
 
+            var write = new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    _rwLock.EnterWriteLock();
+                    var newElement = rnd.Next(1, 50);
+
+                    _collection.Add(newElement);
+                    Console.WriteLine($"Element - {newElement} -  is added;");
+                    _rwLock.ExitWriteLock();
+
+                    var state = _collection.ToArray();
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(ReadCollection), state);
+                }
+            });
+
+            write.Start();
             Console.ReadLine();
+        }
+
+        private static void ReadCollection(object collection) 
+        {
+            var items = (int[])collection;
+            _rwLock.EnterReadLock();
+            Console.WriteLine(string.Join(" ", items));
+            _rwLock.ExitReadLock();
         }
     }
 }
