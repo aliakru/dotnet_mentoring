564267cd95c93f21a64b8925660c57f53a5456bc
Aliaksandr Rusak <Aliaksandr.Rusak@ihsmarkit.com>
2023-04-28 23:38:38 +0200

    task6

--- 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,8 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -24,6 +26,52 @@ namespace MultiThreading.Task6.Continuation
 
             // feel free to add your code
 
+            // A.
+            var mainTask = Task.Run(() => Task.Delay(500));
+
+            var task1 = mainTask.ContinueWith(t => Console.WriteLine("Task a. is running."));
+
+            var failingTask = Task.Run(() => throw new Exception())
+                .ContinueWith(t => Console.WriteLine("Task a. is running after failing main task."));
+
+
+            // B.
+            
+            var parentSuccess = Task.Run(() => Task.Delay(500))
+                .ContinueWith(
+                    t => Console.WriteLine($"Task B. is running after main task. IsFaulted parent: {t.IsFaulted}"),
+                TaskContinuationOptions.OnlyOnFaulted);
+
+            var parentFailing = Task.Run(() => throw new Exception($"Failed expectedly ^_^ (Thread# {Thread.CurrentThread.ManagedThreadId})"))
+                .ContinueWith(
+                    t => Console.WriteLine($"Task B. is running after failing main task. Error from parent: {t.Exception.Message}.Thread: {Thread.CurrentThread.ManagedThreadId}."),
+                TaskContinuationOptions.OnlyOnFaulted);
+
+            // C.
+
+            var parentSuccessC = Task.Run(() => Task.Delay(500))
+                .ContinueWith(
+                t => Console.WriteLine("Task C. is running after main task."),
+                TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+
+            var parentFailingC = Task.Run(() => throw new Exception($"Failed expectedly C Task. Thread# {Thread.CurrentThread.ManagedThreadId}."))
+                .ContinueWith(
+                t => Console.WriteLine($"Task C thread# {Thread.CurrentThread.ManagedThreadId}. Error from parent: {t.Exception.Message}."),
+                TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+
+            // D.
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+            
+            tokenSource.Cancel();
+            var parentFailingD = Task.Run(
+                () => throw new Exception($"Failed expectedly D Task. Is ThreadPool# {Thread.CurrentThread.IsThreadPoolThread}."),
+                token)
+                .ContinueWith(t =>
+                    Console.WriteLine($"Task D is tp# {Thread.CurrentThread.IsThreadPoolThread}. Error from parent: {t.Exception?.Message}."),
+                TaskContinuationOptions.OnlyOnCanceled | TaskContinuationOptions.LongRunning);
+
+            //tokenSource.Cancel();
             Console.ReadLine();
         }
     }
