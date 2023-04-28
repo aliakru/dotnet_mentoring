d5a05109b5ecd3436f11d25caf8619e90fd7833a
Aliaksandr Rusak <Aliaksandr.Rusak@ihsmarkit.com>
2023-04-28 17:11:31 +0200

    Task 4

--- 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
+++ 01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,6 +10,7 @@
  */
 
 using System;
+using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
@@ -28,7 +29,45 @@ namespace MultiThreading.Task4.Threads.Join
 
             // feel free to add your code
 
+            //ThreadRecursiveAction(10);
+            ThreadPoolRecursiveAction(10);
             Console.ReadLine();
+        }
+
+        static void ThreadRecursiveAction(object number)
+        {
+            var id = (int) number;
+            if (id >= 0) 
+            {
+                id--;
+                var t = new Thread(ThreadRecursiveAction);
+                t.Name = $"Thread #{id}";
+
+                t.Start(id);
+                Console.WriteLine(Thread.CurrentThread.Name);
+
+                t.Join();
+            }
+        }
+
+        static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(3);
+
+        static void ThreadPoolRecursiveAction(object number)
+        {
+            var id = (int)number;
+
+            if (id > 0)
+            {
+                id--;
+
+                ThreadPool.QueueUserWorkItem(ThreadPoolRecursiveAction, id);
+            }
+
+            Console.WriteLine($"Thread #{id} is waiting.");
+            semaphoreSlim.Wait();
+            Thread.Sleep(1000/id);
+            Console.WriteLine($"Thread #{id} is started execution.");
+            Console.WriteLine($"Thread #{id} has left.");
         }
     }
 }
